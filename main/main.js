9296071536ad93aa4dad45203975c836d95d2781
youths <552099787@qq.com>
2017-05-31 19:50:58 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -4,23 +4,25 @@ module.exports = function main(inputs) {
 	var goods = loadAllItems();
 	var totalPrice = 0.0;
 	var products=[];
-	for (var indexi = 0; indexi < inputs.length; indexi++; )
+	for(var i = 0; i < inputs.length; i++; )
 	{
 		var num = 0;
-		for (var indexj = 0; indexj < inputs.length; indexj++;){
-			if(inputs[indexi] == inputs[indexj]){
+		for(var j = 0; j < inputs.length; j++;){
+			if(inputs[i] == inputs[j])
+			{
 				num ++;
 			}
 		}
-		var product = {item:inputs[indexi],number:num};
-		if(products.indexOf(product) == -1{
+		var product = {item:inputs[i],number:num};
+		if(products.indexOf(product) == -1)
+		{
 			products.push(product);
 		}
 	} 
 	console.log("'***<没钱赚商店>购物清单***\n'");
-	for (var index = 0; index < products.length ; index++)
+	for(var index = 0; index < products.length ; index++)
 	{
-		for (var tab = 0; tab < goods.length ; tab++)
+		for(var tab = 0; tab < goods.length ; tab++)
 		{
 			if(products[index].item == goods[tab].barcode){
 				var str =  '名称：'+goods[tab].name+'，数量：'+products[index].number+goods[tab].unit'，单价：'+goods[tab].price+'(元)，小计：'+products[index].number*goods[tab].price+'(元)\n'
