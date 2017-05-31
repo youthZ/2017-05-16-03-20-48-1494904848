98955a922a557fcf015496636d644ccc92fd8a84
youths <552099787@qq.com>
2017-05-31 18:15:30 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -3,31 +3,8 @@ const loadAllItems = require('./loadAllItems.js');
 module.exports = function main(inputs) {
 	var goods = loadAllItems();
 	var totalPrice = 0.0;
-	var products;
-	for (var index_i = 0; index_i < inputs.length; index_i++;){
-		var num = 0;
-		for (var index_j = 0; index_j < inputs.length; index_j++;){
-			if(inputs[index_i] == inputs[index_j]){
-				num ++;
-			}
-		}
-		var product = {item:inputs[index_i],number:num};
-		if(products.indexOf(product) == -1{
-			products.push(product);
-		}
-	}
-	console.log("'***<没钱赚商店>购物清单***\n'");
-	for (var index = 0; index < products.length ; index++)
-	{
-		for (var tab = 0; tab < goods.length ; tab++)
-		{
-			if(products[index].item == goods[tab].barcode){
-				var str =  '名称：'+goods[tab].name+'，数量：'+products[index].number+goods[tab].unit'，单价：'+goods[tab].price+'(元)，小计：'+products[index].number*goods[tab].price+'(元)\n'
-				console.log(str);
-				totalPrice += products[index].number*goods[tab].price;
-			}
-		}
-	}
+	var products=[];
+	
 	var text = '----------------------\n' +
             '总计：'+totalPrice+'(元)\n' +
             '**********************';
