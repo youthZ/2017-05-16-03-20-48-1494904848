6a1a54e6f165ce0c42fd058870c18b85b681a8d1
youths <552099787@qq.com>
2017-06-01 09:41:49 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -4,7 +4,7 @@ module.exports = function main(inputs) {
 	var goods = loadAllItems();
 	var totalPrice = 0.0;
 	var products = [];
-	var strText ;
+	var strText = "***<没钱赚商店>购物清单***\n";
 	for(var i = 0; i < inputs.length; i++)
 	{
 		var num = 0;
@@ -20,20 +20,19 @@ module.exports = function main(inputs) {
 			products.push(product);
 		}
 	} 
-	strText ='***<没钱赚商店>购物清单***\n';
 	for(var index = 0; index < products.length ; index++)
 	{
 		for(var tab = 0; tab < goods.length ; tab++)
 		{
 			if(products[index].item == goods[tab].barcode){
-				strText +=  '名称：'+goods[tab].name+'，数量：'+products[index].number+goods[tab].unit+'，单价：'+goods[tab].price+'(元)，小计：'+products[index].number*goods[tab].price+'(元)\n';
+				strText +=  "名称："+goods[tab].name+"，数量："+products[index].number+goods[tab].unit+"，单价："+goods[tab].price+"(元)，小计："+products[index].number*goods[tab].price+"(元)\n";
 				totalPrice += products[index].number*goods[tab].price;
 			}
 		}
 	}
-	strText += '----------------------\n' +
-            '总计：'+totalPrice+'(元)\n' +
-            '**********************';
+	strText += "----------------------\n" +
+            "总计：'+totalPrice+'(元)\n" +
+            "**********************";
     console.log(strText);
     return strText;
 };
